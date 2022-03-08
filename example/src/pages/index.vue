fd54024dd40d9ddab6c640646882978a9559e7b0
Alex.Liu <larch.liu@gmail.com>
2022-03-08 14:03:18 +0800

    chore: update example

--- example/src/pages/index.vue
+++ example/src/pages/index.vue
@@ -20,7 +20,6 @@ const showMesh = ref(true)
       <em text-sm op75>Live real-time avatar</em>
     </p>
 
-    <div py-4 />
     <nav text-xl mt-6 inline-flex gap-2>
       <button class="icon-btn !outline-none" @click="showCam = !showCam">
         <div i-carbon-camera :class="showCam ? '!opacity-100' : '!opacity-40'" />
@@ -28,7 +27,7 @@ const showMesh = ref(true)
       <button class="icon-btn !outline-none" @click="showMesh = !showMesh">
         <div i-carbon-3d-print-mesh :class="showMesh ? '!opacity-100' : '!opacity-40'" />
       </button>
-      <div class="w-1px m-2 opacity-10 bg-current" />
+      <div class="w-1px mx-2 opacity-20 bg-current" />
       <button class="icon-btn !outline-none" @click="toggleDark()">
         <div v-if="isDark" i-carbon-moon />
         <div v-else i-carbon-sun />
