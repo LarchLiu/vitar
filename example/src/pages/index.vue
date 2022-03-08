3b20990a1c9a8309c80bcc742f853c09afd5d7f8
Alex.Liu <larch.liu@gmail.com>
2022-03-08 13:55:30 +0800

    chore: update example

--- example/src/pages/index.vue
+++ example/src/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Vitar } from '@cloudgeek/vitar'
+import { isDark, toggleDark } from '~/composables'
 import '@cloudgeek/vitar/dist/index.css'
 
 const showCam = ref(false)
@@ -27,6 +28,20 @@ const showMesh = ref(true)
       <button class="icon-btn !outline-none" @click="showMesh = !showMesh">
         <div i-carbon-3d-print-mesh :class="showMesh ? '!opacity-100' : '!opacity-40'" />
       </button>
+      <div class="w-1px m-2 opacity-10 bg-current" />
+      <button class="icon-btn !outline-none" @click="toggleDark()">
+        <div v-if="isDark" i-carbon-moon />
+        <div v-else i-carbon-sun />
+      </button>
+
+      <a
+        class="icon-btn"
+        i-carbon-logo-github
+        rel="noreferrer"
+        href="https://github.com/LarchLiu/vitar"
+        target="_blank"
+        title="GitHub"
+      />
     </nav>
   </div>
 </template>
